a3a76723020eccf6930bf83a50a6b6e1c9125b71
encelti <informatica@encel.com.br>
2026-07-11 18:13:12 -0300

    Compile in abb format

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.polypodium.polypodium"
+        // Deve ser idêntico ao package name registrado no Google Play Console.
+        applicationId = "com.zapperbrz.polypodium"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
